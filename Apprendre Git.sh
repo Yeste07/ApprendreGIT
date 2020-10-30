1a7e8d44ca3d306edad3800426f324c9f3b62e85
Yassine Sadellah <Yassine Sadellah>
2020-10-31 00:26:01 +0100

    commit2

--- Apprendre Git.sh
+++ Apprendre Git.sh
@@ -1,8 +1,8 @@
 Git : le control de version
 
 
-1)Creation du dossier de projet sur le local.
-2)Cibler le dossier avec la commande init.
+#1)Creation du dossier de projet sur le local.
+#2)Cibler le dossier avec la commande init.
 
 YESTE@IT-Engineer MINGW64 /
 $ cd D:\PremierProjet
@@ -13,7 +13,7 @@ Initialized empty Git repository in D:/PremierProjet/.git/
 
 YESTE@IT-Engineer MINGW64 /d/PremierProjet (master)
 
-3) to point vers un depot a distant github.
+#3) to point vers un depot a distant github.
 
 YESTE@IT-Engineer MINGW64 /d/PremierProjet (master)
 $ git remote add OPENCLASS https://github.com/OpenClassrooms-Student-Center/ProjetOpenSource.git
@@ -21,7 +21,7 @@ $ git remote add OPENCLASS https://github.com/OpenClassrooms-Student-Center/Proj
 YESTE@IT-Engineer MINGW64 /d/PremierProjet (master)
 $
 
-4) cloner le contenu de projet de depot github vers le dossier local.
+#4) cloner le contenu de projet de depot github vers le dossier local.
 
 YESTE@IT-Engineer MINGW64 /d/PremierProjet (master)
 $ git clone https://github.com/OpenClassrooms-Student-Center/ProjetOpenSource.git
@@ -33,22 +33,20 @@ Receiving objects: 100% (7/7), done.
 YESTE@IT-Engineer MINGW64 /d/PremierProjet (master)
 
 
-5) Apprendre le système branch : creation branch pour toute modification temporaire puis on lapplique sur branch
+#5) Apprendre le système branch : creation branch pour toute modification temporaire puis on lapplique sur branch
    principal master. 
  
- git branch edit   [pour creation de la branch edit]
+ git branch edev   [pour creation de la branch dev]
  git branch        [pour afficher toute les branchs]
- git checkout edit [pour ce deplacer vers la branch edit]
 
- --------------------------------------------------------------
-
-
-YESTE@IT-Engineer MINGW64 /d/Test (master)
+ YESTE@IT-Engineer MINGW64 /d/Test (master)
 $ git branch
   dev
 * master
   preprod
   recette
+
+ git checkout dev [pour ce deplacer vers la branch dev]
 
 YESTE@IT-Engineer MINGW64 /d/Test (master)
 $ git checkout dev
@@ -61,6 +59,8 @@ $ git branch
   master
   preprod
   recette
+
+#6) voir le status Git : phase de preparation de contenu
 
 YESTE@IT-Engineer MINGW64 /d/Test (dev)
 $ git status
@@ -75,6 +75,8 @@ Untracked files:
         PremierFichierDev.txt
 
 no changes added to commit (use "git add" and/or "git commit -a")
+
+#7) ajout de fichier txt : phase numéro 1
 
 YESTE@IT-Engineer MINGW64 /d/Test (dev)
 $ git add PremierFichierDev.txt
@@ -91,6 +93,7 @@ Changes not staged for commit:
   (use "git restore <file>..." to discard changes in working directory)
         deleted:    PremierFichier.txt.txt
 
+#8) commit la modification : phase numero 2
 
 YESTE@IT-Engineer MINGW64 /d/Test (dev)
 $ git commit -m "Creation du fichier de dev"
@@ -98,6 +101,7 @@ $ git commit -m "Creation du fichier de dev"
  1 file changed, 1 insertion(+)
  create mode 100644 PremierFichierDev.txt
 
+#9) push la modification : phase numero 3 (mais avant il faut faire un git remote add ... to specifier le deport distant)
 YESTE@IT-Engineer MINGW64 /d/Test (dev)
 $ git push
 fatal: No configured push destination.
@@ -109,6 +113,7 @@ and then push using the remote name
 
     git push <name>
 
+#10) le merge de la branch dev avec la branch master.(merge= master+modification de dev) 
 
 YESTE@IT-Engineer MINGW64 /d/Test (dev)
 $ git checkout master
@@ -123,24 +128,7 @@ Fast-forward
  1 file changed, 1 insertion(+)
  create mode 100644 PremierFichierDev.txt
 
-YESTE@IT-Engineer MINGW64 /d/Test (master)
-$ git checkout recette
-Switched to branch 'recette'
-D       PremierFichier.txt.txt
-
-YESTE@IT-Engineer MINGW64 /d/Test (recette)
-$ git log
-commit 3c7143a848a2ec7b12854078f4e225e2d90522c2 (HEAD -> recette, preprod)
-Author: Yassine Sadellah <Yassine Sadellah>
-Date:   Fri Oct 30 19:07:32 2020 +0100
-
-    ajout de la balise script
-
-commit 056e94148ac91f2faf92dffff2248e1fd3c679d2
-Author: Yassine Sadellah <Yassine Sadellah>
-Date:   Fri Oct 30 19:01:25 2020 +0100
-
-    first commit
+#11)Supprimer un commit dans la branch master.
 
 YESTE@IT-Engineer MINGW64 /d/Test (recette)
 $ git checkout master
@@ -168,18 +156,6 @@ Date:   Fri Oct 30 19:01:25 2020 +0100
     first commit
 
 YESTE@IT-Engineer MINGW64 /d/Test (master)
-$ git reset hard
-fatal: ambiguous argument 'hard': unknown revision or path not in the working tree.
-Use '--' to separate paths from revisions, like this:
-'git <command> [<revision>...] -- [<file>...]'
-
-YESTE@IT-Engineer MINGW64 /d/Test (master)
-$ git reset hard 3c7143a848a2ec
-fatal: ambiguous argument 'hard': unknown revision or path not in the working tree.
-Use '--' to separate paths from revisions, like this:
-'git <command> [<revision>...] -- [<file>...]'
-
-YESTE@IT-Engineer MINGW64 /d/Test (master)
 $ git reset --hard 3c7143a848a2ec
 HEAD is now at 3c7143a ajout de la balise script
 
@@ -197,15 +173,10 @@ Date:   Fri Oct 30 19:01:25 2020 +0100
 
     first commit
 
-YESTE@IT-Engineer MINGW64 /d/Test (master)
-$ git reset --hard  056e94148ac91f2faf
-HEAD is now at 056e941 first commit
+#12)modifier le message de dernier commit
 
-YESTE@IT-Engineer MINGW64 /d/Test (master)
-$
-
-
-
+git commit -m 'firstCommit'
+git commit --amend - 'stepnumber1' [Cette commande permet de modifier mon message commit]
 
 
 
